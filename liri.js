f12e71db26f824a38cad4f4c2218a777c9fc11fd
Luke Champ <luke.champ@outlook.com>
2018-02-17 15:51:11 -0500

    my tweets working

--- liri.js
+++ liri.js
@@ -3,14 +3,17 @@ var keys = require("./keys.js");
 var Twitter= require("twitter");
 // var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var action = process.argv[2]
 
+if(action === "my-tweets"){
 client.get('statuses/user_timeline', function(error, tweets){
     if (error) throw error;
 
     for (i=0; i < tweets.length; i++){
     console.log (tweets[i].text);}
 
-});
+    });
+}
 
 
 
