3f775d5ab59d954f76c239e2f8b6780b6f713f43
Luke Champ <luke.champ@outlook.com>
2018-02-17 15:47:25 -0500

    twitter working

--- liri.js
+++ liri.js
@@ -1,3 +1,16 @@
 require("dotenv").config();
-var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+var Twitter= require("twitter");
+// var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+client.get('statuses/user_timeline', function(error, tweets){
+    if (error) throw error;
+
+    for (i=0; i < tweets.length; i++){
+    console.log (tweets[i].text);}
+
+});
+
+
+
