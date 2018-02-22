f700d79b42e764b56b2697b769776f6b334241cb
Luke Champ <luke.champ@outlook.com>
2018-02-21 21:00:03 -0500

    added undefined functions and do what it says

--- liri.js
+++ liri.js
@@ -21,11 +21,19 @@ client.get('statuses/user_timeline', function(error, tweets){
 
 
 // SPOTIFY
-if(command === "spotify-this-song"){
+
+else if(command === "spotify-this-song"){
+  
+  if(input === undefined){
+    input = "ace of base";
+  }
+  
 spotify.search({ type: 'track', query: input}, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
+    
+   
 
 
   
@@ -36,14 +44,19 @@ spotify.search({ type: 'track', query: input}, function(err, data) {
   });
 }
 
-// OMDB
-if (command === "movie-this")
-var request= require("request");
 
+
+// OMDB
+else if(command === "movie-this"){
+var request= require("request");
+if(input === undefined){
+  input= "Mr. Nobody";
+}
 request("http://www.omdbapi.com/?t=" + input + "&plot=short&apikey=trilogy", function(error, response, body){
   if (error) {
     return console.log('Error occurred: ' + error);
   }
+  console.log(input)
 
   console.log(JSON.parse(body).Title);
   console.log(JSON.parse(body).Year);
@@ -55,4 +68,35 @@ request("http://www.omdbapi.com/?t=" + input + "&plot=short&apikey=trilogy", fun
   console.log(JSON.parse(body).Actors);
   
 });
+}
+
+else if (command = "do-what-it-says"){
+  var fs = require("fs");
+fs.readFile("random.txt", "utf8", function(error, data) {
+  if (error) {
+    return console.log('Error occurred: ' + error);
+  }
+data = (data.split(","))
+
+  spotify.search({ type: 'track', query:data[1]}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  console.log(data.tracks.items[0].artists[0].name + " noartist");
+  console.log(data.tracks.items[0].album.name + " album");
+  console.log(data.tracks.items[0].name + " name");
+  console.log(data.tracks.items[0].preview_url + " preview URL");  
+  });
+
+
+   
+    
+   
+
+
+  
+ 
+  });
+}
+ 
 
